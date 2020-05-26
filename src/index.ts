9a87a689b306d405f5f56865792e0cf20ca8664b
Lucemans <luc@lucemans.nl>
2020-05-26 03:04:46 +0200

    Health Check and HTML

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import { listenAndServe, ServerRequest } from 'https://deno.land/std/http/server.ts';
 import { existsSync, walkSync, ensureDir, ensureDirSync } from "https://deno.land/std/fs/mod.ts";
 
-const defaultDir = "."; //"/data";
+const defaultDir = "/data";
 ensureDirSync(defaultDir);
 
 listenAndServe({port: 8000}, async (req: ServerRequest) => {
+    if (req.url.match(/\/healthz/g)) {
+        req.respond({status: 200, body: new TextEncoder().encode("200 OK")});
+        return;
+    }
     try {
         let filename = defaultDir + (req.url.startsWith('/') ? '' : '/') + req.url; 
         const raw = req.headers.has('cache-control') ? (filename.startsWith('./raw/')) : true;
@@ -18,7 +22,6 @@ listenAndServe({port: 8000}, async (req: ServerRequest) => {
             if (info.isDirectory) {
                 const files = [];
                 for (const entry of Deno.readDirSync(filename)) {
-                        console.log('found ' + entry.name);
                         files.push(entry.name);
                 }
                 req.respond({body: "Files in this dir: \n" + files.join('\n')});
@@ -28,7 +31,7 @@ listenAndServe({port: 8000}, async (req: ServerRequest) => {
                     req.respond({body: (
                         raw ? 
                         Deno.readFileSync(filename) :
-                        "This is a file"
+                        Deno.readTextFileSync('./src/index.html').replace(/\{\{contentRaw\}\}/g, filename.replace(/^\.\//g, '/raw/'))
                     )});
                 return;
             }

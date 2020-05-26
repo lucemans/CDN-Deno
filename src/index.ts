59d15854b0aa63aeb1f471fc07b11473cafecc79
Lucemans <luc@lucemans.nl>
2020-05-26 04:39:12 +0200

    Improved Efficiency

--- src/index.ts
+++ src/index.ts
@@ -5,35 +5,33 @@ const defaultDir = "/data";
 ensureDirSync(defaultDir);
 
 listenAndServe({port: 8000}, async (req: ServerRequest) => {
-    if (req.url.match(/\/healthz/g)) {
-        req.respond({status: 200, body: new TextEncoder().encode("200 OK")});
-        return;
-    }
     try {
-        let filename = (req.url.startsWith('/') ? '' : '/') + req.url; 
-        const raw = req.headers.has('cache-control') ? (filename.startsWith('/raw/')) : true;
-        
+        let filename = (req.url.startsWith('/') ? '' : '/') + req.url;
+        let raw = false;
         if (filename.startsWith('/raw/')) {
             filename = filename.substr(('/raw').length);
+            raw = true;
+        }
+        if (!raw && !req.headers.has('cache-control')) {
+            raw = true;
         }
         filename = defaultDir + filename;
-
         if (existsSync(filename)) {
             const info = await Deno.stat(filename);
+            if (info.isFile) {
+                req.respond({body: (
+                    raw ? 
+                    Deno.readFileSync(filename) :
+                    Deno.readTextFileSync('./src/index.html').replace(/\{\{contentRaw\}\}/g, filename.replace(defaultDir + '/', '/raw/'))
+                )});
+                return;
+            }
             if (info.isDirectory) {
                 const files = [];
                 for (const entry of Deno.readDirSync(filename)) {
                         files.push(entry.name);
                 }
                 req.respond({body: "Files in this dir: \n" + files.join('\n')});
-                return;
-            }
-            if (info.isFile) {
-                    req.respond({body: (
-                        raw ? 
-                        Deno.readFileSync(filename) :
-                        Deno.readTextFileSync('./src/index.html').replace(/\{\{contentRaw\}\}/g, filename.replace(defaultDir + '/', '/raw/'))
-                    )});
                 return;
             }
             req.respond({body: "found " + filename});
@@ -43,5 +41,9 @@ listenAndServe({port: 8000}, async (req: ServerRequest) => {
     } catch (error) {
         console.log(error);
         req.respond({status: 500})
+    }
+    if (req.url.match(/\/healthz/g)) {
+        req.respond({status: 200, body: new TextEncoder().encode("200 OK")});
+        return;
     }
 });

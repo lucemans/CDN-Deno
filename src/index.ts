1204a3536614610de8d0f8bd961dd17959d37d3a
Lucemans <luc@lucemans.nl>
2020-05-26 02:44:49 +0200

    Full Fledged

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,42 @@
 import { listenAndServe, ServerRequest } from 'https://deno.land/std/http/server.ts';
-import { existsSync } from "https://deno.land/std/fs/mod.ts";
+import { existsSync, walkSync, ensureDir, ensureDirSync } from "https://deno.land/std/fs/mod.ts";
+
+const defaultDir = "/data";
+ensureDirSync(defaultDir);
 
 listenAndServe({port: 8000}, async (req: ServerRequest) => {
     try {
-        let filename = (req.url.startsWith('/') ? '' : '/') + req.url; 
-        
-        req.respond({body: "Hello"});
+        let filename = '.' + (req.url.startsWith('/') ? '' : '/') + req.url; 
+        const raw = req.headers.has('connection') ? (filename.startsWith('./raw/')) : true;
+        if (filename.startsWith('./raw/')) {
+            filename = filename.substr('./raw/'.length);
+        }
+
+        if (existsSync(filename)) {
+            const info = await Deno.stat(filename);
+            if (info.isDirectory) {
+                const files = [];
+                for (const entry of Deno.readDirSync(filename)) {
+                        console.log('found ' + entry.name);
+                        files.push(entry.name);
+                }
+                req.respond({body: "Files in this dir: \n" + files.join('\n')});
+                return;
+            }
+            if (info.isFile) {
+                    req.respond({body: (
+                        raw ? 
+                        Deno.readFileSync(filename) :
+                        "This is a file"
+                    )});
+                return;
+            }
+            req.respond({body: "found " + filename});
+            return;
+        }
+        req.respond({status: 404});
     } catch (error) {
-        req.respond({status: 404})
+        console.log(error);
+        req.respond({status: 500})
     }
 });
